27edb91b614052f0077580cc679164a09569194a
Mads Nielsen <man@praqma.net>
2016-05-31 11:41:16 +0200

    New patch

--- wincrementor.rb
+++ wincrementor.rb
@@ -23,62 +23,6 @@ DOCOPT
 
 begin
 
-  # Defaults
-  major = /\+major/
-  minor = /\+minor/
-  patch = /\+patch/
-
-  # Base value 
-  base = '0.0.0'
-
-  #Increments the major version number based on current, returns new
-  def increment_major(current)
-    puts "Increment major"    
-  end
-
-  def increment_minor(current)    
-    puts "Increment minor"
-  end
-
-  def increment_patch(current)
-    puts "Increment patch"
-  end
-
-  input = Docopt::docopt(doc)
-
-  # Current directory
-  repository = Rugged::Repository.new(Dir.pwd) 
-
-  # Set up rugged to traverse our repoitory
-  head = repository.lookup(repository.head.target.oid)
-  tail = repository.rev_parse(input['<ref>'])
-  w = Rugged::Walker.new(repository)
-  w.sorting(Rugged::SORT_TOPO)    
-  w.push(head)
-
-
-  # Set the intermediate result to the base
-  result = SemVer.new
-
-  w.each do |commit|
-    #If we find the commit. Abort
-    if commit.oid == tail      
-      break
-    end
-
-    if major =~ commit.message
-      result.major += 1
-      result.minor = 0
-      result.patch = 0
-    elsif minor =~ commit.message
-      result.minor += 1
-    else
-      result.patch += 1
-    end
-  end
-
-  puts result.to_s
-  
   # look at all commits between <ref> and HEAD. 
   # Look for commit-message mentions 
   # of +major or +minor (unless different patterns where specified ).
@@ -96,6 +40,64 @@ begin
   # The last should probably be equivalent to just defaulting version_string 
   # to "0.0.0" if none was provided.
 
+
+  input = Docopt::docopt(doc)
+
+  # Defaults (Fo now hardcoded)
+  major = /\+major/
+  minor = /\+minor/
+  patch = /\+patch/
+
+  # Base value 
+  base = '0.0.0'
+
+  if input['<semver_version_string>']
+    base = input['<semver_version_string>']
+  end
+  
+  # Current directory
+  repository = Rugged::Repository.new(Dir.pwd) 
+
+  # Set up rugged to traverse our repoitory
+  head = repository.lookup(repository.head.target.oid)
+  tail = repository.rev_parse(input['<ref>'])
+  w = Rugged::Walker.new(repository)
+  # We need to walk 'backwards'
+  w.sorting(Rugged::SORT_REVERSE)    
+  w.push(head)
+
+  # Set the intermediate result to the base
+  # X.Y.Z-SPECIAL
+  split = base.split('-')
+  v_number = split[0].split('.')
+  s_special = '-'+split[1]
+
+  special = ''
+  if s_special
+    special = s_special
+  end
+
+  result = SemVer.new(v_number[0].to_i, v_number[1].to_i, v_number[2].to_i, special)
+
+  w.each do |commit|
+    puts commit.oid
+    if major =~ commit.message
+      result.major += 1
+      result.minor = 0
+      result.patch = 0
+    elsif minor =~ commit.message
+      result.minor += 1
+      result.patch = 0
+    else
+      result.patch += 1
+    end
+    #If we find the commit. Abort
+    if commit.oid == tail      
+      break
+    end
+  end
+
+  puts result.format "%M.%m.%p%s"
 
 rescue Docopt::Exit => e
   puts "Wincrementor 1.0"
